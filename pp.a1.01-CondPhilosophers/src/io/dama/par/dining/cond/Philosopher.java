d8ed3ac226ad7bb85f7ece89a3b958f73ee70d8d
Michell Skorna <michell.skorna@gmail.com>
2018-10-15 10:41:20 +0200

    Basics 2.

--- pp.a1.01-CondPhilosophers/src/io/dama/par/dining/cond/Philosopher.java
+++ pp.a1.01-CondPhilosophers/src/io/dama/par/dining/cond/Philosopher.java
@@ -4,37 +4,65 @@ import java.util.concurrent.locks.Lock;
 
 public class Philosopher extends Thread implements IPhilosopher {
 
-    @Override
+    private Philosopher left;
+    private Philosopher right;
+    private Lock table;
+    
+    public Philosopher(){
+    	
+    }
+    
+	@Override
     public void setLeft(final IPhilosopher left) {
         // TODO Auto-generated method stub
-
+		this.left = (Philosopher) left;
     }
 
     @Override
     public void setRight(final IPhilosopher right) {
         // TODO Auto-generated method stub
-
+    	this.right = (Philosopher) right;
     }
 
     @Override
     public void setTable(final Lock table) {
         // TODO Auto-generated method stub
-
+    	this.table = table;
     }
 
     @Override
     public void stopPhilosopher() {
         // TODO Auto-generated method stub
-
+    	System.out.println(getId() + " forced to stop.");
+    	this.interrupt();
     }
     
     @Override
     public void run(){
-    	// TODO
+    	// TODO ?
     }
     
     @Override
     public void start(){
-    	// TODO
+    	    	
+    	System.out.println(getId() + " starts eating.");
+    	
+    	if(getRight().isAlive()||getLeft().isAlive())
+			// das funktioniert noch nicht
+    		System.out.println(getId() + " is Interrupted.");
+    	
+    	while(getRight().isAlive()||getLeft().isAlive()){
+    			// das funktioniert noch nicht
+    	}
+    	
+    	System.out.println(getId() + " finished.");
     }
+
+	public Philosopher getRight() {
+		return right;
+	}
+
+	public Philosopher getLeft() {
+		return left;
+	}
 }
