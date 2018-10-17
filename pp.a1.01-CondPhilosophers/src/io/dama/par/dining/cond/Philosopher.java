4164a00115c0fa74e891b5983deb76991807a14e
Marina Hovhannisyan <1725184@stud.hs-mannheim.de>
2018-10-17 08:36:47 +0200

    Konstantin-philosophers Lösung

--- pp.a1.01-CondPhilosophers/src/io/dama/par/dining/cond/Philosopher.java
+++ pp.a1.01-CondPhilosophers/src/io/dama/par/dining/cond/Philosopher.java
@@ -2,69 +2,150 @@ package io.dama.par.dining.cond;
 
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.Random;
 
 public class Philosopher extends Thread implements IPhilosopher {
+	private IPhilosopher left, right;
+	private Lock table;
+	public Condition isNotEating;
+	volatile private boolean isNotStopped = true;
+	volatile Thread selfPhilosopher = this;
+	private Random random = new Random();
+	private int eatCounter = 0;
+	private boolean atBeginning = true;
+	public volatile boolean eating = false;
 
-    private Philosopher left;
-    private Philosopher right;
-    private Lock table;
-    private Condition canEat;
-    private Condition canNotEat;
-    // lock tables, give conditions
-    // whilst eating await to get right condition
-    
-    public Philosopher(){
-    	
-    }
-    
 	@Override
-    public void setLeft(final IPhilosopher left) {
-        // TODO Auto-generated method stub
-		this.left = (Philosopher) left;
-    }
+	public void setLeft(final IPhilosopher left) {
+		this.left = left;
 
-    @Override
-    public void setRight(final IPhilosopher right) {
-        // TODO Auto-generated method stub
-    	this.right = (Philosopher) right;
-    }
-
-    @Override
-    public void setTable(final Lock table) {
-        // TODO Auto-generated method stub
-    	this.table = table;
-    }
-
-    @Override
-    public void stopPhilosopher() {
-        // TODO Auto-generated method stub
-    	System.out.println(getId() + " forced to stop.");
-    	this.interrupt();
-    }
-    
-    @Override
-    public void run(){
-    	// TODO ?
-    }
-    
-    @Override
-    public void start(){
-    	// TODO Either start eat or think
-    }
-
-	public Philosopher getRight() {
-		return right;
 	}
 
-	public Philosopher getLeft() {
-		return left;
+	@Override
+	public void setRight(final IPhilosopher right) {
+		this.right = right;
 	}
-	
-	public void eat(){
-		// eating requires resources
+
+	@Override
+	public void setTable(final Lock table) {
+		this.table = table;
+		isNotEating = table.newCondition(); // Verstößt vermutlich gegen alle
+		// Programmierprinzipien
 	}
-	
-	public void think(){
-		// thinking requires no resources and signals being free
+
+	@Override
+	public void stopPhilosopher() {
+		System.out.println(this.getName() + "Gegessen:" + this.eatCounter);
+		isNotStopped = false;
+		selfPhilosopher.interrupt();
 	}
+
+	@Override
+	public void run() {
+
+		while (isNotStopped) {
+
+			try {
+				think();
+			} catch (InterruptedException e) {
+			}
+			try {
+				eat();
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+
+	private void think() throws InterruptedException {
+		System.out.println(this.getName() + "Gebe Signal");
+		Thread.sleep(this.random.nextInt(PhilosopherExperiment.MAX_THINKING_DURATION_MS));
+	}
+
+	private void eat() throws InterruptedException {
+		table.lock();
+		try {
+			if (atBeginning) {
+				atBeginning = false;
+			} else {
+				((Philosopher) left).isNotEating.await();
+				((Philosopher) right).isNotEating.await();
+			}
+			System.out.println(this.getName() + "Esse");
+			eating = true;
+			if (((Philosopher) left).eating || ((Philosopher) right).eating)
+				System.out.println("FEHLER");
+			this.eatCounter++;
+			Thread.sleep(PhilosopherExperiment.MAX_EATING_DURATION_MS);
+			System.out.println(this.getName() + "Beende Essen");
+			eating = false;
+			this.isNotEating.signalAll();
+		} finally {
+			table.unlock();
+		}
+	}
+
+
+//
+//    private Philosopher left;
+//    private Philosopher right;
+//    private Lock table;
+//    private Condition canEat;
+//    private Condition canNotEat;
+//    // lock tables, give conditions
+//    // whilst eating await to get right condition
+//    
+//    public Philosopher(){
+//    	
+//    }
+//    
+//	@Override
+//    public void setLeft(final IPhilosopher left) {
+//        // TODO Auto-generated method stub
+//		this.left = (Philosopher) left;
+//    }
+//
+//    @Override
+//    public void setRight(final IPhilosopher right) {
+//        // TODO Auto-generated method stub
+//    	this.right = (Philosopher) right;
+//    }
+//
+//    @Override
+//    public void setTable(final Lock table) {
+//        // TODO Auto-generated method stub
+//    	this.table = table;
+//    }
+//
+//    @Override
+//    public void stopPhilosopher() {
+//        // TODO Auto-generated method stub
+//    	System.out.println(getId() + " forced to stop.");
+//    	this.interrupt();
+//    }
+//    
+//    @Override
+//    public void run(){
+//    	// TODO ?
+//    }
+//    
+//    @Override
+//    public void start(){
+//    	// TODO Either start eat or think
+//    }
+//
+//	public Philosopher getRight() {
+//		return right;
+//	}
+//
+//	public Philosopher getLeft() {
+//		return left;
+//	}
+//	
+//	public void eat(){
+//		// eating requires resources
+//	}
+//	
+//	public void think(){
+//		// thinking requires no resources and signals being free
+//	}
 }
